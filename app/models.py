de65ff55b358cad931fb0796cf3be784155fa6dd
Bruno Sousa <bruno@ubuntuserver.(none)>
2012-06-12 15:15:59 +0100

    Fix

--- app/models.py
+++ app/models.py
@@ -6,4 +6,4 @@ class Utilizadores(models.Model):
 	username = models.CharField('username',max_length=80,null=False,blank=False)
 	password = models.CharField('password',max_length=80,null=False,blank=False)
 	email = models.EmailField('email',null=False,blank=False)
-	
+	facebookid = models.IntegerField('facebookid',null=True)
