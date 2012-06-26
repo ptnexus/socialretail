f3657f0e34ecf82e1694f4b8c32269ea4493348d
David Leal <david@debian>
2012-06-26 22:58:13 +0100

    wishlists-list

--- app/views_wishlists.py
+++ app/views_wishlists.py
@@ -1,9 +1,36 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
-from django.shortcuts import render
-from models import CustomUser
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from models import CustomUser,WishList,WishListProduct
+from login import Login,access_required
 import json
+from django.core.paginator import Paginator
+from forms import WishListProductForm
 
 
+@access_required
 def list(request,*kwargs):
-	return render(request, 'facebook/wishlists.html', {},)
+	user = Login(request).getUser()
+	wishlists = []
+	search = ''
+	if 'search' in request.POST and request.POST['search'] != '':
+		search = request.POST['search']
+		wishlists = WishList.objects.filter(user = user, name__contains = search)
+	else:
+		wishlists = WishList.objects.filter(user = user)
+	paginator = Paginator(wishlists, 10)
+	try:
+		page = int(request.GET.get('page',1))
+	except:
+		page = 1
+	if page > paginator.num_pages:
+		page = paginator.num_pages
+	if page < 1:
+		page = 1
+	return render(request, 'facebook/wishlists.html', {
+		'paginator': paginator,
+		'search':search,
+		'page': paginator.page(page),	
+		'page_number':page,
+	},)
